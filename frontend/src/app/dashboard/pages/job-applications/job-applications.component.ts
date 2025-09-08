be31ab741b6b58e2774a0eb7002c8c73b57eebe8
Yo445 <ya108942@gmail.com>
2025-09-09 00:48:12 +0300

    create features folders

---
+++ frontend/src/app/dashboard/pages/job-applications/job-applications.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-job-applications',
+  imports: [],
+  templateUrl: './job-applications.component.html',
+  styleUrl: './job-applications.component.css'
+})
+export class JobApplicationsComponent {
+
+}
---
+++ frontend/src/app/dashboard/pages/market-trends/market-trends.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-market-trends',
+  imports: [],
+  templateUrl: './market-trends.component.html',
+  styleUrl: './market-trends.component.css'
+})
+export class MarketTrendsComponent {
+
+}
