40fdb80ddd6c36389f2067661cc839b2560aceb4
Ahmed Ashraf Ibrahim <ahmedashrafmarketing2024@gmail.com>
2025-09-13 11:16:56 +0300

    update market trend branch

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./components/header/header.component";
+import { NavBottomComponent } from "./components/nav-bottom/nav-bottom.component";
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, NavBottomComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
