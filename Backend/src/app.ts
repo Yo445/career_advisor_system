d0a6b1ac8ac6beaea4b99efd838727bffbadcbed
amaal.yasser <amal.yasser.abdelkader@gmail.com>
2025-11-24 07:16:43 +0200

    mongo DataBase

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -5,7 +5,11 @@ const app = express();
 app.get("/", (req, res) => {
     res.send("Hello from Express");
 });
+//Middleware
+app.use(express.json());
 
 app.listen(8000, () => {
     console.log("Listen on port 8000.");
 });
+
+//export default app;
